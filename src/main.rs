9a0eec254009fbda829262c5920061e6f8f33153
nicoSix <nicolasix@outlook.fr>
2022-01-03 01:31:59 +0100

    Adding logging: can be set up from .env file

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Also use dotenv to get RUST_LOG
+    dotenv::dotenv().expect("Failed to read .env file");
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Renamed!
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -4,6 +4,7 @@ use crate::routes::health_check;
 use crate::routes::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,7 +12,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
-        App::new()
+        App::new()// Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
