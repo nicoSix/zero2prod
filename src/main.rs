4c9e7f81d88572f2dbc4a518a80b34578b534e85
nicoSix <nicolasix@outlook.fr>
2022-01-03 01:32:26 +0100

    Formatting code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 //! main.rs
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
--- src/startup.rs
+++ src/startup.rs
@@ -3,8 +3,8 @@
 use crate::routes::health_check;
 use crate::routes::subscribe;
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -12,7 +12,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Wrap the pool using web::Data, which boils down to an Arc smart pointer
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
-        App::new()// Middlewares are added using the `wrap` method on `App`
+        App::new() // Middlewares are added using the `wrap` method on `App`
             .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
